01ec54971fcfb68f6e80f87cdfd291dfd7106506
isdckft <kovacs.attila@isdc.hu>
2021-06-26 21:30:43 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           >
             Learn React
           </a>
+          <H1> Hello, Welcome to AWS</H1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
